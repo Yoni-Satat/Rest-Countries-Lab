98c0f6fb81fac2b818a1cb76df5b3190cdd1d617
Yoni Satat <yoni.satat@gmail.com>
2017-12-13 12:14:37 +0000

    added function to display selected country info

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
+let countries = [];
+
 const app = function () {
   const url = 'https://restcountries.eu/rest/v2/all';
 
   const button = document.querySelector('#show-countries');
   button.addEventListener('click', function(thing) {
-    makeRequest(url, requestComplete)
+    makeRequest(url, requestComplete);
+  });
+
+  const select = document.querySelector('select');
+  select.addEventListener('change', function() {
+    //
+    const selectedIndex = this.value;
+    countryInfo(countries[selectedIndex]);
   });
 }
 
@@ -20,19 +29,39 @@ const requestComplete = function() {
   if(this.status !== 200) return;
   // console.log(this); to check that the status is 200!
   const jsonString = this.responseText;
-  const countries = JSON.parse(jsonString);
+  countries = JSON.parse(jsonString);
   // console.log(countries);
   populateList(countries);
 }
 
 const populateList = function(countries) {
   const select = document.querySelector('select');
-  countries.forEach(function(country) {
+  countries.forEach(function(country, index) {
     const option = document.createElement('option');
     option.innerText = country.name;
+    option.value = index;
     select.appendChild(option);
   });
 }
+
+const countryInfo = function(country) {
+  const ul = document.querySelector('ul');
+
+  ul.innerHTML = '';
+
+  const name = document.createElement('li');
+  name.innerText = country.name;
+  const capital = document.createElement('li');
+  capital.innerText = country.capital;
+  const population = document.createElement('li');
+  population.innerText = country.population;
+  ul.appendChild(name);
+  ul.appendChild(capital);
+  ul.appendChild(population);
+}
+
+
+
 
 
 
